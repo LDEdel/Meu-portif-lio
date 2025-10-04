47d46aa459e13b42842438d8e6a55461ae9cb4d1
Leonardo Dana Edelsberg <leodanaedel1611@gmail.com>
2025-10-04 20:41:02 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const curriculumButton = document.getElementById("curriculumButton");
+curriculumButton.onclick = function(){
+    window.open("https://docs.google.com/document/d/1nJ_Z5u8ey5rCwTMVyuDK-BseaiB0yWMLHStp7AxBIlg/edit?tab=t.0", "_blank");
+}
+
+const gitButton = document.getElementById("git");
+gitButton.onclick = function(){
+    window.open("https://github.com/LDEdel");
+}
+
+const instagramButton = document.getElementById("instagram");
+instagramButton.onclick = function(){
+    window.open("https://www.instagram.com/leonardo.edelsberg");
+}
+
+const linkedinButtom = document.getElementById("linkedin");
+linkedinButtom.onclick = function(){
+    window.open("https://www.linkedin.com/in/leonardo-dana-edelsberg-8ba48b334");
+}
+
+const facebookButton = document.getElementById("facebook");
+facebookButton.onclick = function(){
+    window.open("https://www.facebook.com/profile.php?id=100016479767523");
+}
